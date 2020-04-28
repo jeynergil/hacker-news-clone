07e439464ba33c246be9b11639efab390e286546
Jeyner Gil Caga <jeynergilcaga@gmail.com>
2020-04-28 14:00:43 +0800

    Updates

---
+++ precache-manifest.363caa15a9099eb0add92dc794ab683d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c598849874a767804ac45c1570895798",
+    "url": "/hacker-news-clone/index.html"
+  },
+  {
+    "revision": "6a701e81e9f256aed2bb",
+    "url": "/hacker-news-clone/static/js/2.88cf350f.chunk.js"
+  },
+  {
+    "revision": "863004d46cd7375f6c043c934b2ed4e3",
+    "url": "/hacker-news-clone/static/js/2.88cf350f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d0ff0f744505d7abb6e",
+    "url": "/hacker-news-clone/static/js/main.edf8f3cd.chunk.js"
+  },
+  {
+    "revision": "494647f775d812ee3fce",
+    "url": "/hacker-news-clone/static/js/runtime-main.1eb8d402.js"
+  }
+]);
